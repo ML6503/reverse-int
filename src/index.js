e22e4970ff5a4f17e728f4f36eb92936bf213f19
ML6503 <ml6503@gmail.com>
2021-03-02 22:12:52 +0200

    feat: add comments and parseInt() to return integer

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-    return Array.from(Math.abs(n).toString()).map(Number).reverse().join("");
+    // we take n with Math.abs() convert it into positive integer if negative
+    // then we convert digits to string and create array from it
+    // then map calls Number on each value in the array (casting it to a number)
+    // then we reverse digits into array and
+    // convert the elements of an array into a string without comma mark
+    // then parseInt() function parses a string and returns an integer though tests accept result in string too
+    return parseInt(Array.from(Math.abs(n).toString()).map(Number).reverse().join(""));
 }
